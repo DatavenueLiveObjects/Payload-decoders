022d04123f48a2c28eda5e92ba155367c01a45c5
Ziaudin Latif <ziaudin.latif@orange.com>
2019-10-21 17:14:47 +0100

    Update decoderWithWholeMessage.js
    Copyright header added

--- lo-js-decoder-test-framework-samples/src/test/resources/javascript/example3/decoderWithWholeMessage.js
+++ lo-js-decoder-test-framework-samples/src/test/resources/javascript/example3/decoderWithWholeMessage.js
@@ -1,5 +1,8 @@
-/**
+/*
+ * Copyright (C) 2019 Orange S.A.
  * 
+ * This software is distributed under the terms and conditions of the '3-Clause BSD License'
+ * license which can be found in the file 'LICENSE.txt' in this package distribution. 
  */
 
 var decode = function(encoded, dataMessage) {
